167e04d8c2a6e098a424572227b185b0061b602c
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-04-05 14:25:22 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 basic.forever(function () {
-    serial.writeLine("DS" + Math.round(dstemp.celsius(DigitalPin.P0) * 100) / 100)
-    serial.writeLine("TM" + Math.round(input.temperature() * 100) / 100)
+    serial.writeLine("" + (Math.round(dstemp.celsius(DigitalPin.P0) * 100) / 100))
     basic.pause(1000)
 })
